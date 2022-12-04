2c031c49c292858a7bc7bd2396355d9eda722f90
garamond13 <98652255+garamond13@users.noreply.github.com>
2022-12-04 23:24:10 +0100

    clamp linearize

--- unsharpMask.glsl
+++ unsharpMask.glsl
@@ -4,7 +4,7 @@
 //!DESC unsharp mask pass0
 
 vec4 hook() {
-    return linearize(textureLod(HOOKED_raw, HOOKED_pos, 0.0));
+    return clamp(linearize(textureLod(HOOKED_raw, HOOKED_pos, 0.0)), 0.0, 1.0);
 }
 
 //!HOOK MAIN
